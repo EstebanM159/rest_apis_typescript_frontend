5d31a4daf4352e4370464068ab5a98c9a47e79bd
EstebanM159 <estebanm91231@gmail.com>
2024-05-09 11:23:38 -0300

    Fix page white

--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,22 @@
 import { Suspense, lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-import Layout from './layouts/Layout'
 import { loader as productsLoader, action as updateAvailabilityAction } from './pages/Products'
 import { action as newProductAction } from './pages/NewProduct'
 import { loader as editProductLoader, action as editProductAction } from './pages/EditProduct'
 import { action as deleteProductAction } from './components/ProductDetails'
 import Spinner from './components/Spinner'
-const Products = lazy(async () => {
-  await new Promise(resolve => setTimeout(resolve, 500)) // 1000ms de retraso
-  return import('./pages/Products')
+const Layout = lazy(async () => {
+  await new Promise(resolve => setTimeout(resolve, 600)) // 1000ms de retraso
+  return import('./layouts/Layout')
 })
+const Products = lazy(async () => await import('./pages/Products'))
 const NewProduct = lazy(async () => await import('./pages/NewProduct'))
 const EditProduct = lazy(async () => await import('./pages/EditProduct'))
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <Suspense fallback={<Spinner/>}><Layout/></Suspense>,
     children: [
       {
         index: true,
